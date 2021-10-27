941120a2b1c59af1e0cbc1a5cc8e0ede0b863e0f
gmzych <gmzych@gmail.com>
2021-10-27 21:50:16 +0200

    few changes at setup file

--- configurationfile.py
+++ configurationfile.py
@@ -1,16 +1,26 @@
 from appium import webdriver
 
 
+def setup_driver():
+    app_name = 'app-debug.apk'
+    app_path = '/Users/michalzych/PycharmProjects/PythonAppiumMapProject/app-debug.apk'
 
-app_name = 'app-debug.apk'
-# zmienna lokalna, do podmiany
-app_path = '/Users/michalzych/PycharmProjects/PythonAppiumMapProject/app-debug.apk'
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['appname'] = app_name
+    desired_caps['app'] = app_path
+    desired_caps['newCommandTimeout'] = 120
 
-desired_caps={}
-desired_caps['platformName']='Android'
-desired_caps['appname']=app_name
-desired_caps['app']=app_path
-desired_caps['newCommandTimeout']=120
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    driver.implicitly_wait(30)
+    return driver
 
-webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+
+
+
+
+
+
+
 
