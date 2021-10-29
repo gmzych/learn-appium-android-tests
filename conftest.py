c69f5dde9b584c8eecf6ac088b5f127d3eafc1c0
gmzych <gmzych@gmail.com>
2021-10-29 12:52:03 +0200

    ready driver setup file

--- conftest.py
+++ conftest.py
@@ -1,7 +1,11 @@
+import pytest
 from appium import webdriver
+#pytest rozpoznaje plik pod nazwą conftest jako plik setupowy
 
+#fixture - klasa która powoduje że mój driver działa w obrębie wszystkich klas
+@pytest.fixture(scope='class')
+def driver(request):
 
-def setup_driver():
     app_name = 'app-debug.apk'
     app_path = '/Users/michalzych/PycharmProjects/PythonAppiumMapProject/app-debug.apk'
 
@@ -11,10 +15,9 @@ def setup_driver():
     desired_caps['app'] = app_path
     desired_caps['newCommandTimeout'] = 120
 
-    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-    driver.implicitly_wait(30)
+    request.cls.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    request.cls.driver.implicitly_wait(30)
     return driver
-
 
 
 
