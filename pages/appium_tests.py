e5f035df7132da3e56173072fc3f1f0e5ea5a0b5
gmzych <gmzych@gmail.com>
2021-11-30 19:24:08 +0100

    few more changes to tests

--- pages/appium_tests.py
+++ pages/appium_tests.py
@@ -27,7 +27,7 @@ class TestObjects:
         assert map.is_displayed()
 # test scrolluj do wybranego tekstu, wybierz i sprawdz czy na mapie wyświetliła się ikona google
     def test_check_if_icon_on_the_map_is_displayed(self):
-        self.main_page.scroll_to_specific_element("Abetone, IT")
+        self.main_page.scroll_to_specific_element('Abetone, IT')
         self.main_page.search_get_result_click('/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[19]/android.widget.TextView')
         icon_on_the_map = self.main_page.map_check_if_icon_isdisplayed()
         assert icon_on_the_map.is_displayed()
@@ -35,6 +35,15 @@ class TestObjects:
     def test_check_if_title_is_displayed(self):
         title = self.main_page.check_if_title_is_displayed('Mobile Assignment')
         assert title.is_displayed()
+# test scrolluj do wybranego tekstu i wybierz miasto nastepnie cofnij,powtorz czynnosc
+    def test_scoll_to_specific_element_and_go_back(self):
+        self.main_page.scroll_to_specific_element('Abanilla, ES')
+        self.main_page.search_get_result_click('/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[14]/android.widget.TextView')
+        self.main_page.get_back_to_main_page()
+        self.main_page.scroll_to_specific_element('Abatskoye, RU')
+        self.main_page.search_get_result_click('/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[18]/android.widget.TextView')
+        self.main_page.get_back_to_main_page()
+        assert self.main_page.main_page_is_displayed()
 
 
 
